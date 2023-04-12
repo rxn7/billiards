ef057636afcda22a1bee97e1fa5d8b3b48eee256
rxn7 <rotthin_dev@protonmail.com>
2023-04-13 01:35:46 +0200

    Tweaked the default game options

--- src/gameOptions.h
+++ src/gameOptions.h
@@ -3,7 +3,7 @@
 struct GameOptions {
     bool vsync = true;
     bool cameraFollowCueBall = false;
-    bool lightFollowMouse = true;
+    bool lightFollowMouse = false;
     bool renderBallVelocity = false;
     bool renderPocket = false;
 };
