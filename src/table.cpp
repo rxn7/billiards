c2d7052fa501de4c95603fa1e015066a7d28710a
rxn7 <rotthin_dev@protonmail.com>
2023-04-13 14:59:54 +0200

    (bugfix) Reduced the u_BallRadiusSqr to fix pixels around balls not being shaded

--- src/table.cpp
+++ src/table.cpp
@@ -21,7 +21,7 @@ Table::Table(const sf::Vector2f &size, const sf::Color &color) : m_Size(size), m
 
 void Table::init() {
     assert(s_AdvancedShader.loadFromFile("assets/shaders/table.frag.glsl", sf::Shader::Fragment));
-    s_AdvancedShader.setUniform("u_BallRadiusSqr", Ball::RADIUS * Ball::RADIUS);
+    s_AdvancedShader.setUniform("u_BallRadiusSqr", Ball::RADIUS * Ball::RADIUS * 0.98f);
 
     assert(s_SimpleShader.loadFromFile("assets/shaders/table.simple.frag.glsl", sf::Shader::Fragment));
 }
