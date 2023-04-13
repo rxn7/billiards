52e2fbdaa46cb245d0782d0a58caaff78f884578
rxn7 <rotthin_dev@protonmail.com>
2023-04-13 16:57:35 +0200

    Fixed building for windows

--- make/windows.mk
+++ make/windows.mk
@@ -8,8 +8,8 @@ LDFLAGS := -L$(SFML_DIR)/lib -lsfml-graphics-s -lsfml-window-s -lsfml-audio-s -l
 
 include make/shared.mk
 
-all: setup copy_vendor_binaries compile
+all: setup copy_dlls compile
 
-copy_vendor_binaries:
-	@echo "Copying vendor binaries..."
-	@cp vendor/*/bin/*.dll $(BIN_DIR)/
+copy_dlls:
+	@echo "Copying dlls..."
+	@cp ./vendor/**/*.dll $(BIN_DIR)
